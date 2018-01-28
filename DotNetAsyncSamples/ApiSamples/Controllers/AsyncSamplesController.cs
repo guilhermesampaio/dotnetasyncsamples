890667be0f774f31157f23a35734b41b9881e387
guilherme.sampaio <guilherme.sampaio@concrete.com.br>
2018-01-27 22:28:10 -0200

    Creating samples with api to demonstrate the difference between synchronous and asynchronous processment

---
+++ DotNetAsyncSamples/ApiSamples/Controllers/AsyncSamplesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSamples.Controllers
+{
+
+    public class AsyncSamplesController : Controller
+    {
+        
+        [HttpGet("/sincrono/{id}")]        
+        public string Get(string id)
+        {
+            var initialThread = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Starting sync request: [{id}] with thread: [{initialThread}]");
+            
+            Thread.Sleep(TimeSpan.FromSeconds(30));
+
+            var finalThread = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Finishing sync request: [{id}] with thread: [{finalThread}]");
+
+            return $"sync request {id}";
+        }
+
+        [HttpGet("/assincrono/{id}")]
+        public async Task<string> Get(int id)
+        {
+            var initialThread = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Starting async request: [{id}] with thread: [{initialThread}]");
+
+            await Task.Delay(TimeSpan.FromSeconds(30));
+
+            var finalThread = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Finishing async request: [{id}] with thread: [{finalThread}]");
+
+            return $"async request {id}";
+        }
+
+       
+    }
+}
