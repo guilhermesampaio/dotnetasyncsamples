e1d92549895a235b576902e0fb39992ef0132b12
guilherme.sampaio <guilherme.sampaio@concrete.com.br>
2018-01-27 22:41:34 -0200

    Samples of async await keywords

--- DotNetAsyncSamples/ConsoleSample/Program.cs
+++ DotNetAsyncSamples/ConsoleSample/Program.cs
@@ -11,123 +11,22 @@ namespace ConsoleSample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Main: Starting program");
+            var asyncClass = new AsyncClass();
 
-            //int worker = 0;
-            //int io = 0;
-            //ThreadPool.GetAvailableThreads(out worker, out io);
+            //Console.WriteLine("Main: Before call asyncClass.AsyncMethodWithAwait()");
+            //asyncClass.AsyncMethodWithAwait();
+            //Console.WriteLine("Main: After call asyncClass.AsyncMethodWithAwait()");
 
-            //Console.WriteLine("Thread pool threads available at startup: ");
-            //Console.WriteLine("   Worker threads: {0:N0}", worker);
-            //Console.WriteLine("   Asynchronous I/O threads: {0:N0}", io);
-
-            //Console.WriteLine("Main: Starting program");
-
-            //var asyncClass = new AsyncClass();
-
-            //Console.WriteLine("Main: Before call asyncClass.GetSomeStrinAsync()");
-
-            ////ExecutionWithAwait(asyncClass);
-
-            //////ExecutionWithoutAwait(asyncClass);
-
-            //asyncClass.WriteFileAsync();
-
-            //Console.WriteLine("Main: After call asyncClass.GetSomeStrinAsync()");
-
-
-            //Console.WriteLine("Before method B");
-            //BDiferente();
-
-
-            var soma = HandleMultipleTasks();
-            Console.WriteLine($"After HandleMultipleTasks");
+            Console.WriteLine("Main: Before call asyncClass.AsyncMethodWithoutAwait()");
+            asyncClass.AsyncMethodWithoutAwait();
+            Console.WriteLine("Main: After call asyncClass.AsyncMethodWithoutAwait()");
 
             Console.ReadLine();
 
             
         }
-
-        static async Task B()
-        {
-            Console.WriteLine("Antes do await C");
-            var resultado = await C();
-            Console.WriteLine($"Resultado: {resultado}");
-            Console.WriteLine("Depois do await C");
-        }
-
-
-        static void BDiferente()
-        {
-            Console.WriteLine("Antes do await C");
-            var resultado = C().ContinueWith((antecedent) => Console.WriteLine("Após execução da tarefa que não foi aguardada!!!!"));
-            Console.WriteLine($"Resultado: {resultado}");
-            Console.WriteLine("Depois do await C");
-        }
-
-
-        static async Task<int> C()
-        {
-
-            await Task.Delay(TimeSpan.FromSeconds(5));
-
-            return 10;
-
-        }
-
-
-        static async Task HandleMultipleTasks()
-        {
-            //Task task1 = Task.Run(() => PerformSomeWork(1));
-            //Task task2 = Task.Run(() => PerformSomeWork(2));
-            //Task task3 = Task.Run(() => PerformSomeWork(3));
-            //Task task4 = Task.Run(() => PerformSomeWork(4));
-            
-            //var tasks = new Task[4] { task1, task2, task3, task4 };
-
-            var tasks2 = new List<Task>();
-
-            for (int i = 0; i <= 3; i++)
-            {
-                tasks2.Add(Task.Run(() => PerformSomeWork(i)));
-            }
-
-            
-
-            Console.WriteLine("Before await all");
-            //await Task.WhenAll(tasks2);
-            await Task.WhenAny(tasks2);
-            Console.WriteLine("After await all");
-        }
-
-        private static void PerformSomeWork(int id)
-        {
-            Console.WriteLine($"Task {id}");
-            //Thread.Sleep(TimeSpan.FromSeconds(1));
-            var currentThread = Thread.CurrentThread.ManagedThreadId;
-            Console.WriteLine($"Current Thread: {currentThread}");
-            
-        }
-
-
-
-
-
-
-        private static async Task ExecutionWithAwait(AsyncClass asyncClass)
-        {
-            await asyncClass.AsyncMethodWithAwait(); // Here the program will pause execution and await the task finishes to proceed the execution.
-
-            //await asyncClass.AsyncMethodWithoutAwait(); // Here the program will not await for the task and will continue execution until "Console.ReadLine()".
-        }
-
-        private static Task ExecutionWithoutAwait(AsyncClass asyncClass)
-        {
-            return Task.Factory.StartNew(() =>
-            {
-                asyncClass.AsyncMethodWithAwait(); // Here the program will pause execution and await the task finishes to proceed the execution.
-                asyncClass.AsyncMethodWithoutAwait(); // Here the program will not await for the task and will continue execution until "Console.ReadLine()".
-            });
-        }
+        
 
     }
 
